1a890992a9c5e35730878e355bff4bbdd6ccb838
server@ubuntu.com <server@ubuntu.com>
2023-05-23 07:11:19 +0000

    VMR Repo Created

---
+++ deps/video_codec_rust-f97981b5e62b3f6b.d
@@ -0,0 +1,5 @@
+/home/ubuntu/video-codec-rust/target/release/deps/video_codec_rust-f97981b5e62b3f6b: src/main.rs
+
+/home/ubuntu/video-codec-rust/target/release/deps/video_codec_rust-f97981b5e62b3f6b.d: src/main.rs
+
+src/main.rs:
---
+++ video_codec_rust.d
@@ -0,0 +1,1 @@
+/home/ubuntu/video-codec-rust/target/release/video_codec_rust: /home/ubuntu/video-codec-rust/src/main.rs
